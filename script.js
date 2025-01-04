313491e9be40e6e0348bfb5ab0ae79361b5b5216
Felipe Alafy <git@felipealafy.net>
2025-01-04 19:55:24 -0300

    General Web Site

--- script.js
+++ script.js
@@ -1,71 +1,70 @@
-var scrollStatus = false;
-
 document.addEventListener('DOMContentLoaded', () => {
-    let age = document.getElementById('age');
-    age.innerHTML = ((new Date()).getFullYear() - 2005).toLocaleString('pt-BR');
-    variousTitles().then(r => 0)
+    let idade = document.getElementById('idade');
+    let periodo = document.getElementById('periodo');
+
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let baseYear = currentYear - 2024;
+    let periodCounter = baseYear * 2;
+    let currentMonth = date.getMonth();
+    console.log(currentMonth);
+
+    if (currentMonth === 0 || currentMonth === 5) {
+        periodCounter++;
+    }
+
+    let baseAge = ((new Date()).getFullYear() - 2005);
+    if (currentMonth < 10) {
+        baseAge--;
+    }
+
+    periodo.innerHTML = periodCounter.toString();
+    idade.innerHTML = baseAge.toLocaleString('pt-BR');
 })
 
-async function variousTitles() {
-    let  welcome_title = document.getElementById('welcome');
-    welcome_title.innerHTML = "";
-    let texts = ["Bem\tVindo(a)!", "Ao\tCurriculum\tVitae", "Do Carrasco!", "Do\tFelipe\tAlafy."]
-    const cursor= "|";
 
-    for (let i = 0; i < texts.length; i++) {
-        for (let l = 0; l < texts[i].length; l++) {
-            let delay_time = 200;
-            await sleep(delay_time).then(() => {
-                welcome_title.innerText += cursor;
-            });
-            await sleep(delay_time).then(() => {
-                welcome_title.innerText = welcome_title.innerText.substring(0, welcome_title.innerText.length - 1);
-                welcome_title.innerText += texts[i][l];
-            })
+function changeMode() {
+    let check = document.getElementById("mode");
+    if (check.checked) {
+        document.body.classList.remove("lightmode-style")
+        document.body.classList.add("darkmode-style");
+
+        for (let i = 0; i < document.body.children.length; i++) {
+            console.log(document.body.children[i].tagName === "ASIDE");
+            if (document.body.children[i].tagName === "ASIDE" || document.body.children[i].tagName === "ARTICLE") {
+                document.body.children[i].style.boxShadow = "20px 20px 60px #363636, -20px -20px 60px #4a4a4a";
+            }
+            for (let j = 0; j < document.body.children[i].children.length; j++) {
+                if (document.body.children[i].children[j].tagName === "ARTICLE") {
+                    document.body.children[i].children[j].style.boxShadow = "20px 20px 60px #363636, -20px -20px 60px #4a4a4a";
+                }
+                if (document.body.children[i].children[j].tagName === "ASIDE") {
+                    document.body.children[i].children[j].style.boxShadow = "20px 20px 60px #363636, -20px -20px 60px #4a4a4a";
+                }
+            }
         }
-        await sleep(2000);
-        if (i === texts.length - 1) {break}
-        for (let l = welcome_title.innerText.length; l > 0; l--) {
-            welcome_title.innerText += cursor;
-            await sleep(200).then(() => {
-                welcome_title.innerText = welcome_title.innerText.substring(0, welcome_title.innerText.length - 2);
-            });
+
+        document.body.style.backgroundColor = "#404040FF";
+        document.body.style.color = "#FFFFFF";
+    }
+    if (!check.checked) {
+        document.body.classList.remove("darkmode-style");
+        document.body.classList.add("lightmode-style")
+        for (let i = 0; i < document.body.children.length; i++) {
+            console.log(document.body.children[i].tagName === "ASIDE");
+            if (document.body.children[i].tagName === "ASIDE" || document.body.children[i].tagName === "ARTICLE") {
+                document.body.children[i].style.boxShadow = "20px 20px 60px #bebebe, -20px -20px 60px #ffffff";
+            }
+            for (let j = 0; j < document.body.children[i].children.length; j++) {
+                if (document.body.children[i].children[j].tagName === "ARTICLE") {
+                    document.body.children[i].children[j].style.boxShadow = "20px 20px 60px #bebebe, -20px -20px 60px #ffffff";
+                }
+                if (document.body.children[i].children[j].tagName === "ASIDE") {
+                    document.body.children[i].children[j].style.boxShadow = "20px 20px 60px #bebebe, -20px -20px 60px #ffffff";
+                }
+            }
         }
+        document.body.style.backgroundColor = "#FFFFFF";
+        document.body.style.color = "#000000";
     }
 }
-
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-async function ScrollOnPress() {
-    const button = document.getElementById("startButton");
-    let scroll;
-    let margin;
-    let transform;
-    const baseScroll = 600;
-    if (scrollStatus) {
-        scroll = baseScroll * (-1);
-        scrollStatus = false;
-        margin = "0px";
-        transform = "rotate(-270deg)";
-    } else {
-        scroll = baseScroll;
-        scrollStatus = true;
-        margin = "250px"
-        transform = "rotate(-90deg)";
-    }
-
-    button.style.marginTop = margin;
-    window.scrollBy({left: 0,top: scroll, behavior: 'smooth'});
-    await sleep(1000);
-    button.style.translate = "0";
-    await sleep(400);
-    button.style.transform = transform;
-
-    button.addEventListener("mouseover", function (event) {
-        transform += " scale(1.2)";
-        button.style.transform = transform;
-    })
-}
-
